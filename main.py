a4f74e08d029f2c5836d9184c8f7edab65f10b51
Thiago Felipe Ribeiro Brito <britoff02@gmail.com>
2025-08-13 22:26:02 -0300

    Projeto

---
+++ main.py
@@ -0,0 +1,12 @@
+import streamlit as st
+import pandas as pd
+
+
+@st.cache_data
+def carregar_dados(dados):
+    tabela = pd.read_csv()
+
+
+st.title('Projeto Analise Empresarial')
+st.write("Bem vindo, ...")
+
