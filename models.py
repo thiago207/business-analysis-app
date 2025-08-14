0b09aaa429d0c176da8655ced7ee413aa684304c
Thiago Felipe Ribeiro Brito <britoff02@gmail.com>
2025-08-14 18:34:38 -0300

    Create models.py

---
+++ models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import create_engine, Integer, String, Boolean, Column
+from sqlalchemy.orm import create_session, declarative_base
+
+
+#class User
+    #id
+    #nome
+    #email
+    #senha
+    #admin
+
+
