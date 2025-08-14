4f3be1194d69eeb033891614e79f7703a5d99c8d
Thiago Felipe Ribeiro Brito <britoff02@gmail.com>
2025-08-13 22:30:36 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,12 +3,13 @@ import pandas as pd
 
 
 @st.cache_data
-def carregar_dados(dados):
-    tabela = pd.read_csv("Base.xlsx")
+def carregar_dados():
+    tabela = pd.read_excel(r"C:\Users\Pichau\Documents\estudos\projeto-analise-empresarial-app\business-analysis-app\Base.xlsx")
     return tabela
 
-
+base = carregar_dados()
 
 st.title('Projeto Analise Empresarial')
 st.write("Bem vindo, ...")
+st.table(base)
 
