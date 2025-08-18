6a4a928969b6ce050379285d5a1fb601a04453df
Thiago Felipe Ribeiro Brito <britoff02@gmail.com>
2025-08-18 20:52:40 -0300

    iamgwens

--- home.py
+++ home.py
@@ -3,21 +3,20 @@ import streamlit as st
 #containers
 #columns
 
-coluna_esquerda, coluna_direita = st.columns([1, 1.5])
+coluna_esquerda, coluna_direita = st.columns([1, 2])
 
-st.title('App Empresarial')
-st.write(f'''
-#### Bem vindo, nome! 
-''')
 
-botao_dashboard = st.button('Dashboards Projeto')
+with coluna_esquerda:
+    st.title("App Empresarial")
+    st.write("Bem vindo, Thiago!")  # aqui você pode usar o nome do login
+    botao_dashboard = st.button("Dashboards Projeto")
+    botao_indicadores = st.button("Indicadores Projeto")
 
-botao_indicadores = st.button('Indicadores Projeto')
+with coluna_direita:
+    st.image('imagens/home_png.png', use_column_width=True)
 
 if botao_dashboard:
     st.switch_page('dashboard.py')
 
 elif botao_indicadores:
     st.switch_page('indicadores.py')   
-
-coluna_direita.image('')
