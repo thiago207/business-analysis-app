09ad3b2df43a661d26ab33426a6f1b91ffe5e496
Thiago Felipe Ribeiro Brito <britoff02@gmail.com>
2025-08-18 20:23:09 -0300

    Update home.py

--- home.py
+++ home.py
@@ -1,3 +1,16 @@
 import streamlit as st
 
-st.write('Teste, home')
+st.title('App Empresarial')
+st.write(f'''
+#### Bem vindo, nome! 
+''')
+
+botao_dashboard = st.button('Dashboards Projeto')
+
+botao_indicadores = st.button('Indicadores Projeto')
+
+if botao_dashboard:
+    st.switch_page('dashboard.py')
+
+elif botao_indicadores:
+    st.switch_page('indicadores.py')
