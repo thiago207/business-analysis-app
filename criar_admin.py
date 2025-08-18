e69fcbd5399d776ea1db106e7b0e10dd5b2b37fc
Thiago Felipe Ribeiro Brito <britoff02@gmail.com>
2025-08-18 19:43:38 -0300

    Update criar_admin.py

--- criar_admin.py
+++ criar_admin.py
@@ -4,4 +4,4 @@ import streamlit_authenticator as stauth
 senha_criptografada = stauth.Hasher(['123123']).generate()[0]
 usuario = User(nome='thiago', email='thiago@gmail.com', senha=senha_criptografada, admin=True)
 session.add(usuario)
-session.commit()        
+session.commit()      
