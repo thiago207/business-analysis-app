95dd72e335eebf8e5c5f435ca556e4e7b04fc66c
Thiago Felipe Ribeiro Brito <britoff02@gmail.com>
2025-08-18 22:08:34 -0300

    Update indicadores.py

--- indicadores.py
+++ indicadores.py
@@ -18,12 +18,25 @@ def card(icone, numero, texto, coluna_card):
 
 coluna_esquerda, coluna_direita, coluna_meio = st.columns([1, 1, 1])
 
-card('oportunidades.png', '2633', 'Oportunidades', coluna_esquerda)
-card('projetos_fechados.png', '504', 'Projetos Fechados', coluna_meio)
-card('em_andamento.png', '331', 'Em Andamento', coluna_direita)
 
-card('total_pago.png', 'R$10,489,500', 'Total Orcado', coluna_esquerda)
-card('total_pago.png', 'R$9,625,100', 'Total Pago', coluna_meio)
-card('desconto.png', 'R$864,400', 'Total Desconto', coluna_direita)
+base_fechados = dados[dados["Status"].isin(["Finalizado", "Em andamento"])]
+
+andamento = dados[dados["Status"] == "Em andamento"]
+
+card('oportunidades.png', f'{dados['Código Projeto'].count():,}', 'Oportunidades', coluna_esquerda)
+
+card('projetos_fechados.png', f'{base_fechados['Código Projeto'].count():,}', 'Projetos Fechados', coluna_meio)
+
+card('em_andamento.png', f'{andamento['Código Projeto'].count():,}', 'Em Andamento', coluna_direita)
+
+
+
+card('total_pago.png', f'R${base_fechados["Valor Orçado"].sum():,}', 'Total Orcado', coluna_esquerda)
+
+card('total_pago.png', f'R${base_fechados["Valor Negociado"].sum():,}', 'Total Pago', coluna_direita)
+
+card('desconto.png', f'R${base_fechados["Desconto Concedido"].sum():,}', 'Total Desconto', coluna_meio)
+
 
 st.table(dados.head(15))
+
