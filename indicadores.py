58f302665a3fb82a536ad8b8d8b1d8873719c1ea
Thiago Felipe Ribeiro Brito <britoff02@gmail.com>
2025-08-18 21:07:13 -0300

    .

--- indicadores.py
+++ indicadores.py
@@ -3,3 +3,8 @@ from carregar_dados import carregar_dados
 st.title('Indicadores')
 
 dados = carregar_dados()
+
+st.table(dados.head(5))
+
+def card(icone, numero, texto, coluna_card):
+    conteiner = coluna_card.container(border=True)
