edc92dc94f77c67ff773a4559dfe867ba6b1ec02
Thiago Felipe Ribeiro Brito <britoff02@gmail.com>
2025-08-18 22:45:16 -0300

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,3 +1,16 @@
 import streamlit as st
-
+from carregar_dados import carregar_dados
 st.title('DashBoards')
+
+base = carregar_dados()
+coluna_esquerda, coluna_direita, coluna_meio = st.columns([1, 1, 1])
+
+
+
+
+setor = coluna_esquerda.selectbox('Setor', list(base['Setor'].unique()))
+status = coluna_meio.selectbox('Status', list(base['Status'].unique()))
+
+base = base[(base['Setor'] == setor) & (base['Status'] == status)]
+
+st.table(base.head(10))
