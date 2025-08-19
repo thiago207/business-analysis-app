9ebaa7749ba109174def28b88cccb6acdc3a4abc
Thiago Felipe Ribeiro Brito <britoff02@gmail.com>
2025-08-18 21:16:43 -0300

    Update indicadores.py

--- indicadores.py
+++ indicadores.py
@@ -4,7 +4,18 @@ st.title('Indicadores')
 
 dados = carregar_dados()
 
-st.table(dados.head(5))
 
 def card(icone, numero, texto, coluna_card):
     conteiner = coluna_card.container(border=True)
+
+    coluna_esquerda, coluna_direita = conteiner.columns([1, 2])
+
+    coluna_esquerda.image(f'imagens/{icone}')
+    coluna_direita.write(numero)
+    coluna_direita.write(texto)
+
+coluna_esquerda, coluna_direita, coluna_meio = st.columns([1, 1, 1])
+
+card('\oportunidades.png', '123', 'Oportunidades', coluna_esquerda)
+card('\oportunidades.png', '123', 'Projetos Fechados', coluna_meio)
+card('\oportunidades.png', '123', 'Em Andamento', coluna_direita)
