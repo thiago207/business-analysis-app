05a3a45ac9968f8970fc7e505b185669f59c8eda
Thiago Felipe Ribeiro Brito <britoff02@gmail.com>
2025-08-18 22:08:45 -0300

    Update indicadores.py

--- indicadores.py
+++ indicadores.py
@@ -24,17 +24,11 @@ base_fechados = dados[dados["Status"].isin(["Finalizado", "Em andamento"])]
 andamento = dados[dados["Status"] == "Em andamento"]
 
 card('oportunidades.png', f'{dados['Código Projeto'].count():,}', 'Oportunidades', coluna_esquerda)
-
 card('projetos_fechados.png', f'{base_fechados['Código Projeto'].count():,}', 'Projetos Fechados', coluna_meio)
-
 card('em_andamento.png', f'{andamento['Código Projeto'].count():,}', 'Em Andamento', coluna_direita)
 
-
-
 card('total_pago.png', f'R${base_fechados["Valor Orçado"].sum():,}', 'Total Orcado', coluna_esquerda)
-
 card('total_pago.png', f'R${base_fechados["Valor Negociado"].sum():,}', 'Total Pago', coluna_direita)
-
 card('desconto.png', f'R${base_fechados["Desconto Concedido"].sum():,}', 'Total Desconto', coluna_meio)
 
 
